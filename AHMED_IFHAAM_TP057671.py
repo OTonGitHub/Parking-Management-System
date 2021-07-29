d513ff95fa1fe5d4480482079c86ca17f9d5ed80
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2021-07-29 15:28:58 +0500

    add assignParkingLot

--- AHMED_IFHAAM_TP057671.py
+++ AHMED_IFHAAM_TP057671.py
@@ -4,6 +4,12 @@
 # RESTRICTIONS
 # SCOPE: GLOBAL VARIABLES NOT ALLOWED
 # OR ON "GLOBAL" Keyword in Functions ?
+
+# TODO
+# test.txt missing case
+# non integer for int
+# regex for time
+
 from time import sleep
 from os import system, name
 from abc import ABC, abstractmethod
@@ -19,14 +25,23 @@ def clear():
     _ = system('clear')
 
 # custom-defined Exceptions
+
+
 class InvalidChoiceError(Exception):
     pass
+
+
 class InvalidVehicleError(Exception):
     pass
+
+
 class InvalidRegistrationNumberError(Exception):
     pass
+
+
 class InvalidTimeError(Exception):
     pass
+
 
 class Vehicle:
     pass
@@ -38,6 +53,7 @@ class Car:
 
 class Van:
     pass
+
 
 class Motorbike:
     pass
@@ -61,14 +77,14 @@ def accountsMenu():
     # TODO Filter by Custom Amount, Default 8
     print("0 : Back to Main Menu")
 
-    choice = evaluateChoice(min = 0, max = 2)
+    choice = evaluateChoice(min=0, max=2)
 
     if choice == 1:
         with open("test.txt", 'r') as file:
             for line in file.readlines():
                 print(line)
         input("\nPress any key to continue..")
-    elif choice == 2:#no need elif, can use if
+    elif choice == 2:  # no need elif, can use if
         pass
     clear()
 
@@ -80,7 +96,7 @@ def searchMenu():
     print("0 : Back to Main Menu")
     # TODO Filter Search by Custom Hour, Default 3
 
-    choice = evaluateChoice(min = 0, max = 2)
+    choice = evaluateChoice(min=0, max=2)
     clear()
 
 
@@ -107,7 +123,7 @@ def vehicleTypeVerification() -> str:
     while(not validated):
         try:
             vehicle = str(input("Vechicle Type (C/V/M) : "))
-            if vehicle[0]!='C' and vehicle[0]!='c' and vehicle[0]!='V' and vehicle[0]!='v' and vehicle[0]!='M' and vehicle[0] != 'm':
+            if vehicle[0] != 'C' and vehicle[0] != 'c' and vehicle[0] != 'V' and vehicle[0] != 'v' and vehicle[0] != 'M' and vehicle[0] != 'm':
                 raise InvalidVehicleError(
                     f"Please Enter Correct Vehicle Type, [Car, Van, Motorbike]\n")
         except InvalidVehicleError as Error:
@@ -123,9 +139,10 @@ def verifyRegistrationNumber() -> str:
         try:
             regNumber = str(input("Vehicle Registraion Number : "))
             if len(regNumber) == 0 or len(regNumber) > 10:
-                raise InvalidRegistrationNumberError("Invalid Registration Number\n")
+                raise InvalidRegistrationNumberError(
+                    "Invalid Registration Number\n")
         except InvalidRegistrationNumberError as error:
-                print(error)
+            print(error)
         else:
             validated = True
     return regNumber
@@ -137,7 +154,8 @@ def verifyCheckInTime() -> int:
         try:
             inTime = int(input("Enter Check-In Time <- [00:00] - [23:59] : "))
             if inTime > 2359 or inTime < 0:
-                raise InvalidTimeError("Error: Enter a number between 0 & 2359\n")
+                raise InvalidTimeError(
+                    "Error: Enter a number between 0 & 2359\n")
         except InvalidTimeError as error:
             print(error)
         else:
@@ -149,28 +167,38 @@ def isFileEmpty(filename: str) -> bool:
     fileDir = os.path.dirname(__file__)  # <-- absolute dir the script is in
     rel_path = filename
     abs_file_path = os.path.join(fileDir, rel_path)
-    #removes absolute path errors
-    #TODO remove after testing
-        #TODO add reference to python doc & so
+    # removes absolute path errors
+    # TODO remove after testing
+    # TODO add reference to python doc & so
 
-    with open(abs_file_path, 'r') as file:
+    with open(abs_file_path, 'r+') as file:  # no file case
         line = file.readline()
-        print(line[0])
-        if line[0] != '{':
+        if not line:
             return True
     return False
 
 
 def assignParkingLot() -> int:
-    if isFileEmpty("test.txt"):
-        return 1
-    listDictionary = mightyFileDictionaryToListDictionary()
     try:
-        with open("test.txt", 'r') as file:
-            pass
+        if isFileEmpty("test.txt"):
+            return 1
+        lotList = []
+        with open("test.txt", 'r'):
+            listDictionary = mightyFileDictionaryToListDictionary()
+            for line in listDictionary:
+                lotList.append(int(line["lot"]))
+        #lotList = list(lotList)
+        #print(lotList)
+        for x in range(1, 21):
+            #print("Checking Lot.. ", x)
+            lot = x
+            if lot not in lotList:
+                return lot
+        else:
+            return -1
     except FileNotFoundError as error:
-        print(error,"\nThere was an error creating 'test.txt'")
-    return 0
+        print(error, "\nThere was an error creating 'test.txt'")
+
 
 def mightyFileDictionaryToListDictionary() -> list:
     try:
@@ -179,14 +207,14 @@ def mightyFileDictionaryToListDictionary() -> list:
             for line in file.readlines():
                 lineDict: dict = {}
                 line = line[1:(len(line)-2)]
-                    #-2 to negate \n char in file
+                # -2 to negate \n char in file
                 line = line.translate(str.maketrans('', '', string.whitespace))
-                    #.replace() with timeit() proved 3 times slower
+                # .replace() with timeit() proved 3 times slower
                 line = line.replace("'", '')
                 pattern = re.compile(r"([\w]+):([A-Za-z0-9-]+)")
-                    #pattern = re.compile(r"('[\w]+'):([A-Za-z0-9]+|'[A-Za-z0-9]+')")
-                    #TODO fix '-' in group(2)
-                    #TODO include re.IGNORECASE flag
+                #pattern = re.compile(r"('[\w]+'):([A-Za-z0-9]+|'[A-Za-z0-9]+')")
+                # TODO fix '-' in group(2)
+                # TODO include re.IGNORECASE flag
                 matches = pattern.finditer(line)
                 for match in matches:
                     (key, value) = match.group(1), match.group(2)
@@ -197,21 +225,28 @@ def mightyFileDictionaryToListDictionary() -> list:
     return lineList
 
 
-def vehicleCheckIn() -> dict:
+def vehicleCheckIn():
     clear()
-    print("""
-            VEHICLE CHECK-IN MENU
-            *********************\n""")
+    print("""VEHICLE CHECK-IN MENU
+*********************\n""")
+    # TODO move assigning first
+    parkingLot: int = assignParkingLot()
+    if parkingLot == -1:
+        print("Warning: ALL LOTS OCCUPIED!")
+        return None
+    elif parkingLot <= 10:  # TODO DEF FUNCTION
+        print("|--> FLOOR : 1 | LOT : ", parkingLot,"\n")
+    elif parkingLot > 10:
+        print("|--> FLOOR : 2 | LOT : ", parkingLot,"\n")
     vehicle: str = vehicleTypeVerification()
     vehi_RegNo: str = verifyRegistrationNumber()
     checkInTime: int = verifyCheckInTime()
-    parkingLot: int = assignParkingLot()
-    vehicleDict = {"lot" : parkingLot,
-                    "registrationNumber" : vehi_RegNo,
-                    "vehicleType" : vehicle,
-                    "checkInTime" : checkInTime,
-                    "checkOutTime" : None,
-    }
+    vehicleDict = {"lot": parkingLot,
+                   "registrationNumber": vehi_RegNo,
+                   "vehicleType": vehicle,
+                   "checkInTime": checkInTime,
+                   "checkOutTime": None,
+                   }
     if vehicle == 'C':
         pass
     elif vehicle == 'V':
@@ -230,8 +265,10 @@ while True:
     choice = mainMenu()
     if choice == 1:
         vehicle = vehicleCheckIn()
-        writeToFile(line = vehicle)
-        input("\nPress any key to continue..")
+        if vehicle == None:
+            input("\nPress any key to continue..")
+        else:
+            writeToFile(line=vehicle)
         clear()
     elif choice == 2:
         print("You have chosen option 2")
